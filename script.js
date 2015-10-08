62d2d746d669358d0ce46015f84fa30e3573dd71
Andreas Andersson <bambismurfen@gmail.com>
2015-10-08 20:26:11 +0200

    Fungerande Header och sök-variabel hämtning. Search= söksida/mall -HTML, Javascript=script, CSS=style.

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+function myFunction() {
+		
+		var rate= $("#test6").val();
+		var year= $("#test4").val();
+		var genre= $("#test2").val();
+		var search = document.getElementById("title").value;
+		document.getElementById("search_title").innerHTML = genre+", "+year+", "+rate+", "+search;
+		
+		$("#test2").val("");
+		$("#test4").val("");
+		$("#test6").val("");
+
+		$("#test2").text("Genre");
+		$("#test4").text("Year");
+		$("#test6").text("Rating");
+		$("#title").val("");
+
+	}
+						 
+$(function fd(){
+
+	$("#test a").click(function fd(){
+
+	$("#test2").text($(this).text());
+	$("#test2").val($(this).text());
+	});
+
+});
+
+$(function fds(){
+
+	 $("#test3 a").click(function fds(){
+
+	$("#test4").text($(this).text());
+	$("#test4").val($(this).text());
+	});
+
+});
+					
+$(function fdsd(){
+
+	$("#test5 a").click(function fdsd(){
+
+	$("#test6").text($(this).text());
+	$("#test6").val($(this).text());
+					    
+					     	
+	});
+
+});
+					
