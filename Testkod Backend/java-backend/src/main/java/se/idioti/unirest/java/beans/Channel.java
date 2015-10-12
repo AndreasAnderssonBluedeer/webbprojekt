ab21a114f704a7b2eda9b973f614826d604dee6b
Andreas Andersson <bambismurfen@gmail.com>
2015-10-12 21:19:21 +0200

    Testkoder för backend med Java och Spark.

---
+++ Testkod Backend/java-backend/src/main/java/se/idioti/unirest/java/beans/Channel.java
@@ -0,0 +1,25 @@
+package se.idioti.unirest.java.beans;
+
+public class Channel {
+	private String name;
+	private int id;
+	
+	public Channel() {
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ Testkod Backend/java-backend/src/main/java/se/idioti/unirest/java/beans/Envelope.java
@@ -0,0 +1,17 @@
+package se.idioti.unirest.java.beans;
+
+public class Envelope {
+	private Playlist playlist;
+
+	public Envelope() {
+		
+	}
+	
+	public Playlist getPlaylist() {
+		return playlist;
+	}
+
+	public void setPlaylist(Playlist playlist) {
+		this.playlist = playlist;
+	}
+}
